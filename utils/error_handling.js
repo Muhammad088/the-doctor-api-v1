7cc4e0f37bad5829804f239e5680c727542116fe
Muhammad088 <mohammad.eid.hassan@gmail.com>
2023-11-19 10:42:55 +0200

    uploading2

--- utils/error_handling.js
+++ utils/error_handling.js
@@ -11,7 +11,9 @@ const errorHandling = (err, req, res, next) => {
   } else {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
     });
   }
 };
